14a7a998c26ff7cf9e25a0f3274c393561f1625a
kyuki3rain <kyuki3rain@gmail.com>
2023-05-22 15:04:24 +0000

    READMEなど追加

---
+++ README.md
@@ -0,0 +1,21 @@
+# 立ち上げ方１
+
+1. node.jsを導入
+
+2. （初回のみ）npm install
+
+3. npm run start
+
+# 立ち上げ方2
+
+1. vscodeを導入
+
+2. dockerを導入
+
+3. dev containerを導入
+
+4. vscodeの中からdev containerを立ち上げる
+
+5. （初回のみ）npm install
+
+6. npm run start
--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,41 @@
 import * as line from '@line/bot-sdk';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
 require('dotenv').config();
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN as string;
 const LINE_TOKEN = process.env.LINE_BOT_TOKEN as string;
 const TARGET_GROUP_ID = process.env.TARGET_GROUP_ID as string;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildEmojisAndStickers,
+		GatewayIntentBits.GuildIntegrations,
+		GatewayIntentBits.GuildWebhooks,
+		GatewayIntentBits.GuildInvites,
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildPresences,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.DirectMessages,
+		GatewayIntentBits.DirectMessageReactions,
+		GatewayIntentBits.DirectMessageTyping,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildScheduledEvents,
+	],
+	partials: [
+		Partials.User,
+		Partials.Channel,
+		Partials.GuildMember,
+		Partials.Message,
+		Partials.Reaction,
+		Partials.GuildScheduledEvent,
+		Partials.ThreadMember,
+	],
+}); //clientインスタンスを作成する
 
 const line_client = new line.Client({
     channelAccessToken: LINE_TOKEN,
@@ -16,25 +45,45 @@ client.on('ready', () => {
   console.log(`Logged in as ${client?.user?.tag}!`);
 });
 
-//返答
 client.on('messageCreate', message => {
     if (message.author.bot) {
         return;
     }
 
+    console.log(message.thread);
+
+    console.log(message.thread?.messageCount);
+    
     const line_message: line.TextMessage = {
         text: message.content,
         type: 'text',
-      };
+        };
 
-      line_client
+        line_client
         .pushMessage(TARGET_GROUP_ID, line_message)
         .then(() => {
-          console.log('Replied to the message!');
+            console.log('Replied to the message!');
         })
         .catch((err) => {
             console.log(err);
         });
 });
+
+// client.on('threadCreate', message => {
+//     console.log(message.messages);
+//     const line_message: line.TextMessage = {
+//         text: `${message.name}\n${message.messages}`,
+//         type: 'text',
+//       };
+
+//       line_client
+//         .pushMessage(TARGET_GROUP_ID, line_message)
+//         .then(() => {
+//           console.log('Replied to the message!');
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//         });
+// });
 
 client.login(TOKEN);
